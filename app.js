a18fba02198c05e23720219ce135ddf0756bbd2e
KhalidZn <khalidzennouhi@gmail.com>
2017-03-28 22:56:28 +0100

    adding more infos

--- app.js
+++ app.js
@@ -228,28 +228,7 @@ function getMovieDetail(userId, field) {
             }
 
             if(field=='poster'){
-                sendMessage(userId, {
-                    "attachment":{
-                        "type":"template",
-                        "payload":{
-                            "template_type":"generic",
-                            "elements":[
-                                {
-                                    "title":"Welcome to Peter\'s Hats",
-                                    "image_url":"https://petersfancybrownhats.com/company_image.png",
-                                    "subtitle":"We\'ve got the right hat for everyone.",
-                                    "default_action": {
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                                        "messenger_extensions": true,
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                }
-                            ]
-                        }
-                    }
-
+                sendMessage(userId, {"text":"poster clicked"
 
                 });
 
